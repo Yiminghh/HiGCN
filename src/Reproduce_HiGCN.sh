29f740acfdc6818361cb523f4ec28c53e3099a69
hym <yimingh999@outlook.com>
2023-01-25 21:20:45 +0800

    2023.1.25

--- src/Reproduce_HiGCN.sh
+++ src/Reproduce_HiGCN.sh
@@ -15,16 +15,12 @@ python train_model.py --RPMAX 100 --net HiGCN --dataset photo --lr 0.1 --alpha 0
 
 # Below is for heterophily datasets
 
-python train_model.py --RPMAX 100 --net HiGCN --dataset chameleon --lr 0.08 --alpha 0.4 --weight_decay 0.0 --dprate 0.85 --dropout 0.65 --early_stopping 400
-#python train_model.py --RPMAX 50 --net HiGCN --dataset chameleon --lr 0.08 --alpha 0.4 --weight_decay 0.0 --dprate 0.85 --dropout 0.65 --early_stopping 400
+python train_model.py --RPMAX 100 --net HiGCN --dataset chameleon --lr 0.1 --alpha 0.4 --weight_decay 0.0 --dprate 0.8 --dropout 0.7 --early_stopping 500
 
-python train_model.py --RPMAX 100 --net HiGCN --dataset film --lr 0.03 --alpha 0.4 --weight_decay 0.0 --dprate 0.7 --dropout 0.9 --early_stopping 500
+python train_model.py --RPMAX 50 --net HiGCN --dataset film --lr 0.01 --alpha 0.5 --weight_decay 0.001 --dprate 0.8 --dropout 0.8 --early_stopping 500
 
 python train_model.py --RPMAX 100 --net HiGCN --dataset squirrel --lr 0.3 --alpha 1.0 --weight_decay 0.0  --dprate 0.6 --dropout 0.6 --early_stopping 500
 
-python train_model.py --RPMAX 100 --net HiGCN --dataset squirrel --lr 0.4 --alpha 0.2 --weight_decay 0.0  --dprate 0.5 --dropout 0.7 --early_stopping 400
+python train_model.py --RPMAX 50 --net HiGCN --dataset texas --lr 0.1 --alpha 0.8 --weight_decay 0.0001 --dprate 0.6 --dropout 0.6 --early_stopping 500
 
-python train_model.py --RPMAX 100 --net HiGCN --dataset texas --lr 0.1 --alpha 0.5 --weight_decay 0.0001 --dprate 0.7 --dropout 0.7
-
-python train_model.py --RPMAX 100 --net HiGCN --dataset wisconsin --lr 0.2 --alpha 0.7 --weight_decay 0.0005 --dprate 0.6 --dropout 0.2 --early_stopping 400
-
+python train_model.py --RPMAX 50 --net HiGCN --dataset wisconsin --lr 0.2 --alpha 0.7 --weight_decay 0.0001 --dprate 0.6 --dropout 0.6 --early_stopping 500
