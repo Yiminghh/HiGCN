7d70ee13ccbedb03354fc2eae3edf5f7a443ca97
hym <yimingh999@outlook.com>
2023-01-25 21:49:11 +0800

    2023.1.25

--- src/Reproduce_HiGCN.sh
+++ src/Reproduce_HiGCN.sh
@@ -17,10 +17,10 @@ python train_model.py --RPMAX 100 --net HiGCN --dataset photo --lr 0.1 --alpha 0
 
 python train_model.py --RPMAX 100 --net HiGCN --dataset chameleon --lr 0.1 --alpha 0.4 --weight_decay 0.0 --dprate 0.8 --dropout 0.7 --early_stopping 500
 
-python train_model.py --RPMAX 50 --net HiGCN --dataset film --lr 0.01 --alpha 0.5 --weight_decay 0.001 --dprate 0.8 --dropout 0.8 --early_stopping 500
+python train_model.py --RPMAX 100 --net HiGCN --dataset film --lr 0.01 --alpha 0.5 --weight_decay 0.001 --dprate 0.8 --dropout 0.8 --early_stopping 500
 
 python train_model.py --RPMAX 100 --net HiGCN --dataset squirrel --lr 0.3 --alpha 1.0 --weight_decay 0.0  --dprate 0.6 --dropout 0.6 --early_stopping 500
 
-python train_model.py --RPMAX 50 --net HiGCN --dataset texas --lr 0.1 --alpha 0.8 --weight_decay 0.0001 --dprate 0.6 --dropout 0.6 --early_stopping 500
+python train_model.py --RPMAX 100 --net HiGCN --dataset texas --lr 0.1 --alpha 0.8 --dprate 0.6 --dropout 0.6 --early_stopping 500
 
-python train_model.py --RPMAX 50 --net HiGCN --dataset wisconsin --lr 0.2 --alpha 0.7 --weight_decay 0.0001 --dprate 0.6 --dropout 0.6 --early_stopping 500
+python train_model.py --RPMAX 100 --net HiGCN --dataset wisconsin --lr 0.2 --alpha 0.7 --dprate 0.6 --dropout 0.6 --early_stopping 500
